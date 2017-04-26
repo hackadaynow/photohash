1462a15954ef94cb6c2fe1d2ebf1b26ed3b08b5b
John Jiang <qjiang@roku.com>
2017-04-26 13:56:38 -0700

    fix indent

--- photohash/photohash.py
+++ photohash/photohash.py
@@ -22,11 +22,11 @@ def average_hash(image_path, hash_size=8):
     """ Compute the average hash of the given image. """
     with open(image_path, 'rb') as f:
         return average_hash_object(f, hash_size)
-    
+
 def average_hash_url(url, hash_size=8):
     """ Compute the average hash of the given image url. """
-        return average_hash_object(StringIO(requests.get(url).content), hash_size)
-    
+    return average_hash_object(StringIO(requests.get(url).content), hash_size)
+
 def average_hash_object(file_object, hash_size=8):
     """ Compute the average hash of the given image file object. """
     # Open the image, resize it and convert it to black & white.
